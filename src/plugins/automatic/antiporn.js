49755413a968ac70e7b2541c606ebaf6ef3fee0d
al-ex-dev <alexitodevs@gmail.com>
2025-03-11 17:46:21 -0500

    Update antiporn.js

--- src/plugins/automatic/antiporn.js
+++ src/plugins/automatic/antiporn.js
@@ -1,23 +1,23 @@
 const nsfwjs = require('nsfwjs')
-// import * as tf from '@tensorflow/tfjs-node';
-import fs from 'fs';
-import path from 'path';
-import os from 'os';
-import ffmpeg from 'fluent-ffmpeg';
-import ffmpegStatic from 'ffmpeg-static';
+const tf = require('@tensorflow/tfjs')
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
+import ffmpeg from 'fluent-ffmpeg'
+import ffmpegStatic from 'ffmpeg-static'
 
-ffmpeg.setFfmpegPath(ffmpegStatic);
+ffmpeg.setFfmpegPath(ffmpegStatic)
 
 export default {
     start: async (m, { sock, db }) => {
-        if (!db.data.chats[m.from]?.antiporn) return;
-        if (!['imageMessage', 'videoMessage', 'stickerMessage'].includes(m.type)) return;
+        if (!db.data.chats[m.from]?.antiporn) return
+        if (!['imageMessage', 'videoMessage', 'stickerMessage'].includes(m.type)) return
         try {
-            let buffer = await m.download();
+            let buffer = await m.download()
             if (m.type === 'videoMessage') {
-                const tmpVideo = path.join(os.tmpdir(), `${m.id}.mp4`);
-                const tmpImage = path.join(os.tmpdir(), `${m.id}.jpg`);
-                fs.writeFileSync(tmpVideo, buffer);
+                const tmpVideo = path.join(os.tmpdir(), `${m.id}.mp4`)
+                const tmpImage = path.join(os.tmpdir(), `${m.id}.jpg`)
+                fs.writeFileSync(tmpVideo, buffer)
                 await new Promise((resolve, reject) => {
                     ffmpeg(tmpVideo)
                         .screenshots({
@@ -33,8 +33,8 @@ export default {
                 fs.unlinkSync(tmpVideo)
                 fs.unlinkSync(tmpImage)
             }
-            const model = await nsfwjs.load();
-            const imgTensor = tf.node.decodeImage(buffer, 3);
+            const model = await nsfwjs.load()
+            const imgTensor = tf.node.decodeImage(buffer, 3)
             const predictions = await model.classify(imgTensor)
             imgTensor.dispose()
             if (predictions.some(p => ['Porn', 'Hentai'].includes(p.className) && p.probability > 0.5)) {
@@ -44,7 +44,7 @@ export default {
                 });
             }
         } catch (error) {
-            console.error('Error en detector NSFW:', error);
+            console.error('Error en detector NSFW:', error)
         }
     }
 }
